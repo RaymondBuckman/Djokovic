561d76924e998eff53f2737116ec415134a16683
Raymond <dude92128@aol.com>
2017-10-11 22:45:26 -0700

    Updated Sidebar.js with bracket animation

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -8,31 +8,139 @@ class Sidebar extends Component {
             <ul className="sidebar-nav">
                 <div id="coat-of-arms"></div>
                 <li>
-                    <a href="#titles-section">Titles <span className="brackets">&#10092;&#10092;&#10092;</span></a>
+                    <a href="#titles-section">
+                        <span className="brackets">
+                            <span className="bracket-3">&#10093;</span>
+                            <span className="bracket-2">&#10093;</span>
+                            <span className="bracket-1">&#10093; </span>
+                        </span>
+                        <span className="first-letter">T</span>itles 
+                        <span className="brackets">
+                            <span className="bracket-1"> &#10092;</span>
+                            <span className="bracket-2">&#10092;</span>
+                            <span className="bracket-3">&#10092;</span>
+                        </span>
+                    </a>
                 </li>
                 <li>
-                    <a href="#movement-section">Movement <span className="brackets">&#10092;&#10092;&#10092;</span></a>
+                    <a href="#movement-section">
+                        <span className="brackets">
+                            <span className="bracket-3">&#10093;</span>
+                            <span className="bracket-2">&#10093;</span>
+                            <span className="bracket-1">&#10093; </span>
+                        </span>
+                        <span className="first-letter">M</span>ovement
+                        <span className="brackets">
+                            <span className="bracket-1"> &#10092;</span>
+                            <span className="bracket-2">&#10092;</span>
+                            <span className="bracket-3">&#10092;</span>
+                        </span>
+                    </a>
                 </li>
                 <li>
-                    <a href="#backhand-section">Backhand <span className="brackets">&#10092;&#10092;&#10092;</span></a>
+                    <a href="#backhand-section">
+                        <span className="brackets">
+                            <span className="bracket-3">&#10093;</span>
+                            <span className="bracket-2">&#10093;</span>
+                            <span className="bracket-1">&#10093; </span>
+                        </span>
+                        <span className="first-letter">B</span>ackhand
+                        <span className="brackets">
+                            <span className="bracket-1"> &#10092;</span>
+                            <span className="bracket-2">&#10092;</span>
+                            <span className="bracket-3">&#10092;</span>
+                        </span>
+                    </a>
                 </li>
                 <li>
-                    <a href="#return-section">Return <span className="brackets">&#10092;&#10092;&#10092;</span></a>
+                    <a href="#return-section">
+                        <span className="brackets">
+                            <span className="bracket-3">&#10093;</span>
+                            <span className="bracket-2">&#10093;</span>
+                            <span className="bracket-1">&#10093; </span>
+                        </span>
+                        <span className="first-letter">R</span>eturn
+                        <span className="brackets">
+                            <span className="bracket-1"> &#10092;</span>
+                            <span className="bracket-2">&#10092;</span>
+                            <span className="bracket-3">&#10092;</span>
+                        </span>
+                    </a>
                 </li>
                 <li>
-                    <a href="#forehand-section">Forehand <span className="brackets">&#10092;&#10092;&#10092;</span></a>
+                    <a href="#forehand-section">
+                        <span className="brackets">
+                            <span className="bracket-3">&#10093;</span>
+                            <span className="bracket-2">&#10093;</span>
+                            <span className="bracket-1">&#10093; </span>
+                        </span>
+                        <span className="first-letter">F</span>orehand
+                        <span className="brackets">
+                            <span className="bracket-1"> &#10092;</span>
+                            <span className="bracket-2">&#10092;</span>
+                            <span className="bracket-3">&#10092;</span>
+                        </span>
+                    </a>
                 </li>
                 <li>
-                    <a href="#serve-section">Serve <span className="brackets">&#10092;&#10092;&#10092;</span></a>
+                    <a href="#serve-section">
+                        <span className="brackets">
+                            <span className="bracket-3">&#10093;</span>
+                            <span className="bracket-2">&#10093;</span>
+                            <span className="bracket-1">&#10093; </span>
+                        </span>
+                        <span className="first-letter">S</span>erve
+                        <span className="brackets">
+                            <span className="bracket-1"> &#10092;</span>
+                            <span className="bracket-2">&#10092;</span>
+                            <span className="bracket-3">&#10092;</span>
+                        </span>
+                    </a>
                 </li>
                 <li>
-                    <a href="#grit-section">Grit <span className="brackets">&#10092;&#10092;&#10092;</span></a>
+                    <a href="#grit-section">
+                        <span className="brackets">
+                            <span className="bracket-3">&#10093;</span>
+                            <span className="bracket-2">&#10093;</span>
+                            <span className="bracket-1">&#10093; </span>
+                        </span>
+                        <span className="first-letter">G</span>rit
+                        <span className="brackets">
+                            <span className="bracket-1"> &#10092;</span>
+                            <span className="bracket-2">&#10092;</span>
+                            <span className="bracket-3">&#10092;</span>
+                        </span>
+                    </a>
                 </li>
                 <li>
-                    <a href="#coach-section">Coach <span className="brackets">&#10092;&#10092;&#10092;</span></a>
+                    <a href="#coach-section">
+                        <span className="brackets">
+                            <span className="bracket-3">&#10093;</span>
+                            <span className="bracket-2">&#10093;</span>
+                            <span className="bracket-1">&#10093; </span>
+                        </span>
+                        <span className="first-letter">C</span>oach
+                        <span className="brackets">
+                            <span className="bracket-1"> &#10092;</span>
+                            <span className="bracket-2">&#10092;</span>
+                            <span className="bracket-3">&#10092;</span>
+                        </span>
+                    </a>
                 </li>
                 <li>
-                    <a href="#gear-section">Gear <span className="brackets">&#10092;&#10092;&#10092;</span></a>
+                    <a href="#gear-section">
+                        <span className="brackets">
+                            <span className="bracket-3">&#10093;</span>
+                            <span className="bracket-2">&#10093;</span>
+                            <span className="bracket-1">&#10093; </span>
+                        </span>
+                        <span className="first-letter">G</span>ear 
+                        <span className="brackets">
+                            <span className="bracket-1"> &#10092;</span>
+                            <span className="bracket-2">&#10092;</span>
+                            <span className="bracket-3">&#10092;</span>
+                        </span>
+                    </a>
                 </li>
             </ul>
             <div id="profile-image"></div>
