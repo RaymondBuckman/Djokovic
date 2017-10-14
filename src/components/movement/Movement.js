c3d042ff11b3630bd639c3690c5a74a2634ad00c
Raymond <dude92128@aol.com>
2017-10-13 23:52:22 -0700

    Increased accessibility

--- src/components/movement/Movement.js
+++ src/components/movement/Movement.js
@@ -21,7 +21,7 @@ export default class Movement extends Component {
                 <div className="container-fluid">
                     <div className="row">
                         <YouTube videoId="VUiBA7ivzCU" opts={opts} id="movement-video" className="video-fade-in" onReady={this._onReady}/>   
-                        <h1 id="movement-main-header" className="text-center">Movement</h1>
+                        <h1 id="movement-main-header" className="text-center" role="heading">Movement</h1>
                         <h2 id="movement-secondary-header" className="text-center">The wall</h2>
                     </div>
                 </div>
@@ -31,7 +31,7 @@ export default class Movement extends Component {
                     <li className="flex-item">
                         <ul className="flex-container-2">
                             <li className="flex-item">
-                                <h1 id="you-shall-not-pass">You Shall Not Pass!</h1>
+                                <h1 id="you-shall-not-pass" tabIndex="0">You Shall Not Pass!</h1>
                                 <p id="movement-paragraph">Novak Djokovic is one of the fastest players to have ever played the sport. His quickness is nearly second-to-none. Also a factor is his tremendous flexibility. When Djokovic slides to get the ball, many times he's almost doing the splits, yet can recover nearly instanteously to retreive the next shot. These two aspects (the raw speed in getting to the ball & the quick recovery from the slide) make it nearly impossible to get the ball past Novak, often forcing the opponent to hit 5 or 6 perfect shots just to win one point. Doing this constently for two, three, or four hours straight is virtually impossible which is why winning against Djokovic is such a daunting task.</p>           
                             </li>
                             <li className="flex-item"></li>
