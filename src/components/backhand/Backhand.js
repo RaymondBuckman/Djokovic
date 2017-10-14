bff0663bf77cdc617b90948337f413819802b0ed
Raymond <dude92128@aol.com>
2017-10-13 23:48:18 -0700

    Increased accessibility

--- src/components/backhand/Backhand.js
+++ src/components/backhand/Backhand.js
@@ -21,7 +21,7 @@ export default class Backhand extends Component {
                 <div className="container-fluid">
                     <div className="row">
                         <YouTube videoId="PVRJgHewyFY" opts={opts} id="backhand-video" className="video-fade-in" onReady={this._onReady}/>   
-                        <h1 id="backhand-main-header" className="text-center">Backhand</h1>
+                        <h1 id="backhand-main-header" className="text-center" role="heading">Backhand</h1>
                         <h2 id="backhand-secondary-header" className="text-center">Approach at your own peril</h2>
                     </div>
                 </div>
@@ -33,7 +33,7 @@ export default class Backhand extends Component {
                         <ul className="flex-container">
                             <li className="flex-item"></li>
                             <li className="flex-item">
-                                <h1 id="power-accuracy">Power + Accuracy</h1>
+                                <h1 id="power-accuracy" tabIndex="0">Power + Accuracy</h1>
                                 <p id="backhand-paragraph">The Djokovic two-handed backhand is considered by many to be the best backhand in the history of tennis. What makes it so good? The combination of pinpoint accuracy, deceptive power, & rock-solid reliability. Novak can consistently place the ball anywhere on the court at will. Even as a baseliner, Novak can generate incredible angles off a hard-hit ball & clip the line with a passing shot. The lengths of time that he can produce these shots over and over again creates havok for Novak's opponents. Also keeping his opponent on their toes is Novak's dropshot. Disguising the shot until the very last moment, Djokovic can take his hand off at the last minute & place the ball barely over the net. Game. Set. Match.  </p>           
                             </li>
                         </ul>
