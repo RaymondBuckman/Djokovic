4c94fcdcbe268035b4a80c948eb7b5bb862df261
RaymondBuckman <raymondbuckman@yahoo.com>
2017-10-18 23:52:24 -0700

    Down arrow added
    A down arrow present in Intro-1. Clicking it takes the user to Intro-2. Also, a new html structure in Intro-2 allows for easier flex-box targeting. Eventually, this structure will be carried throughout the site.

--- src/components/intro/Intro.js
+++ src/components/intro/Intro.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import {findDOMNode} from 'react-dom';
 import ReactTooltip from 'react-tooltip';
 import YouTube from 'react-youtube';
+import downArrow from '../../img/down-arrow.png';
 
 export default class Intro extends Component {
     render() {
@@ -23,18 +24,19 @@ export default class Intro extends Component {
                         <YouTube videoId="4U9I5gHHWvU" opts={opts} id="intro-video" onReady={this._onReady}/>   
                         <h1 id="intro-main-header" className="text-center" role="heading">Novak Djokovic</h1>
                         <h2 id="intro-secondary-header" className="text-center">Man meets machine</h2>
+                        <img id="down-arrow" src={downArrow} height="35px" className="text-center"></img>
                     </div>
                 </div>
             </div>
-            <div className="Intro-2">
+            <div id="arrow-dest" className="Intro-2">
                 <ul className="flex-container-1">
-                    <li className="flex-item">
+                    <li className="flex-item" id="belgrade-flexbox">
                         <h1 id="belgrade-serbia">Belgrade, Serbia</h1>
                     </li>
                     <li className="flex-item">
                         <ul className="flex-container-2">
-                            <li className="flex-item"></li>
-                            <li className="flex-item">
+                            <li className="inner-flex-item"></li>
+                            <li className="inner-flex-item">
                                 <h1 id="the-origin-of-a-champion">The Origin of a Champion</h1>
                                 <p id="origin-paragraph">Novak Djokovic was born on 22 May 1987 in Belgrade, SR Serbia, Yugoslavia, to parents Srđan and Dijana (née Žagar). Novak's father is Montenegrin in origin and his mother is Croatian by background, with Novak considering himself to be Serbian. His two younger brothers, Marko and Djordje, are also tennis players with professional aspirations. A resident in Monte Carlo, Djokovic was coached by former Slovak tennis player Marián Vajda from 2006 until Boris Becker took over the role of head coach in December 2013. Djokovic is a self-described fan of languages, speaking Serbian, English, French, German, and Italian.</p>           
                             </li>
