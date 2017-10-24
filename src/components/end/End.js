c7719dfca7b84ae414827babb3c440f6e7657c6e
Raymond <dude92128@aol.com>
2017-10-23 23:16:09 -0700

    Bootstrap 4 + favicon

--- src/components/end/End.js
+++ src/components/end/End.js
@@ -9,8 +9,10 @@ export default class End extends Component {
             <div className="End-1">
                 <div className="container-fluid" id="one-container">
                     <div className="row">   
-                        <h1 id="end-main-header" className="text-center" role="heading">Idemo Nole!</h1>
-                        <h2 id="end-secondary-header" className="text-center">Let's go Nole!</h2>
+                        <h1 id="end-main-header" className="mx-auto" role="heading">Idemo Nole!</h1>
+                    </div>
+                    <div className="row">
+                        <h2 id="end-secondary-header" className="mx-auto">Let's go Nole!</h2>
                     </div>
                 </div>
             </div>
@@ -18,3 +20,4 @@ export default class End extends Component {
         );
     }
 }
+
