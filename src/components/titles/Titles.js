18bc643014ce043eef83d55086fac7320e1d0584
Raymond <dude92128@aol.com>
2017-12-08 23:57:00 -0800

    Update Titles.js

--- src/components/titles/Titles.js
+++ src/components/titles/Titles.js
@@ -11,49 +11,49 @@ export default class Titles extends Component {
                     <li className="flex-item" id="ao-titles-flexbox">
                         <a href="https://ausopen.com/" target="_blank" 
                         aria-label="go to the Australian Open's website" role="link">
-                            <h1 id="ao-header">6 Australian Open Titles</h1>
+                            <h1 id="ao-header">A.O. Titles - 6</h1>
                         </a>
                     </li>
                     <li className="flex-item" id="fo-titles-flexbox">
                         <a href="http://www.rolandgarros.com/en_FR/index.html" target="_blank" 
                         aria-label="view more Uniqlo products on their website" role="link">
-                            <h1 id="fo-header">1 French Open Title</h1>
+                            <h1 id="fo-header">French Open Titles - 1</h1>
                         </a>
                     </li>
                     <li className="flex-item" id="w-titles-flexbox">
                         <a href="http://www.wimbledon.com/index.html" target="_blank" 
                         aria-label="view more Uniqlo products on their website" role="link">
-                            <h1 id="w-header">3 Wimbledon Titles</h1>
+                            <h1 id="w-header">Wimbledon Titles - 3</h1>
                         </a>
                     </li>
                     <li className="flex-item" id="uso-titles-flexbox">
                         <a href="http://www.usopen.org/index.html" target="_blank" 
                         aria-label="view more Uniqlo products on their website" role="link">
-                            <h1 id="uso-header">2 US Open Titles</h1>
+                            <h1 id="uso-header">US Open Titles - 2</h1>
                         </a>
                     </li>
                     <li className="flex-item" id="1000-flexbox">
                         <a href="https://en.wikipedia.org/wiki/ATP_World_Tour_Masters_1000" target="_blank" 
                         aria-label="View more info on Masters 1000 tournaments" role="link">
-                            <h1 id="atp-1000-header">30 ATP Masters 1000 Titles</h1>
+                            <h1 id="atp-1000-header">Masters 1000 Titles - 30</h1>
                         </a>
                     </li>
                     <li className="flex-item" id="500-flexbox">
                         <a href="https://en.wikipedia.org/wiki/ATP_World_Tour_500_series" target="_blank" 
                         aria-label="View more info on ATP 500 tournaments" role="link">
-                            <h1 id="atp-500-header">12 ATP 500 Titles</h1>
+                            <h1 id="atp-500-header">ATP 500 Titles - 12</h1>
                         </a>
                     </li>
                     <li className="flex-item" id="250-flexbox">
                         <a href="https://en.wikipedia.org/wiki/ATP_World_Tour_250_series" target="_blank" 
                         aria-label="View more info on ATP 250 Series tournaments" role="link">
-                            <h1 id="atp-250-header">9 ATP 250 Titles</h1>
+                            <h1 id="atp-250-header">ATP 250 Titles - 9</h1>
                         </a>
                     </li>
                     <li className="flex-item" id="money-flexbox">
                         <a href="http://www.atpworldtour.com/en/players/novak-djokovic/d643/overview" target="_blank" 
                         aria-label="check prize money & other player info on the official ATP website" role="link">
-                            <h1 id="money-header">$109,805,403 in prize money</h1>
+                            <h1 id="money-header">Prize money - $109,805,403</h1>
                         </a>
                     </li>
                 </ul>
